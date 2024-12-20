f9faedd60b704d98a30a667f43d25047c8079e3d
Santiago Correa <correasanti1997@gmail.com>
2024-12-20 09:12:51 -0300

    vuelta atras con la cadena de conexion

--- dominio/PokemonWeb/Default.aspx
+++ dominio/PokemonWeb/Default.aspx
@@ -13,7 +13,7 @@
                             <img src="<%# string.IsNullOrEmpty((string)Eval("UrlImagen")) || !Eval("UrlImagen").ToString().Contains("http") ? "https://imgs.search.brave.com/TvImnNqSmkLvWLy9Y1Hkith2FQJECMibPyhZ122wNb0/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/dmVjdG9yLWdyYXRp/cy9pbHVzdHJhY2lv/bi1jb25jZXB0by1j/YXJwZXRhLWltYWdl/bmVzXzExNDM2MC0x/MTQuanBnP3NlbXQ9/YWlzX2h5YnJpZA" : Eval("UrlImagen") %>" class="card-img-top img-fluid" alt="<%#Eval("Nombre") %>">
                             <div class="card-body d-flex flex-column">
                                 <h5 class="card-title"><%#Eval("Nombre") %></h5>
-                                <p class="card-text text-truncate" style="max-height: 4em;"><%#Eval("Descripcion") %></p>
+                                <p class="card-text text-truncate" style="max-height: 4em;"><strong>Nro. </strong><%#Eval("Numero") %></p>
                                 <asp:Button runat="server" ID="btnVerPokemon" CssClass="btn btn-primary mt-auto" Text="Ver Detalle" CommandArgument='<%#Eval("Id") %>' CommandName="IdPokemon" OnClick="btnVerPokemon_Click"/>
                             </div>
                         </div>
--- dominio/negocio/script_db/POKEDEX_DB.sql
+++ dominio/negocio/script_db/POKEDEX_DB.sql
@@ -78,6 +78,11 @@ insert into ELEMENTOS values ('Agua')
 
 insert into pokemons values (1, 'Bulbasaur', 'Este Pokémon nace con una semilla en el lomo.', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png', 1, 2, null, 1)
 insert into pokemons values (4, 'Charmander', 'Pokemon de fuego', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png', 2, 3, null, 1)
-insert into pokemons values (11, 'Butterfree', 'mariposa', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png', 1, 1, null, 1)
-insert into pokemons values (15, 'Pidgey', 'Voladorrrrr', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png', 2, 1, null, 1)
+insert into pokemons values (12, 'Butterfree', 'Adora el nectar de las flores', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png', 1, 2, null, 1)
+insert into pokemons values (16, 'Pidgey', 'Dócil y volador, se defiende levantando arena', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png', 1, 3, null, 1)
+insert into pokemons values (7, 'Squirtle', 'Tras nacer, se le hincha el lomo', 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png', 3, 1, null, 1)
+insert into pokemons values (10, 'Caterpie', 'Para protegerse, despide un hedor horrible', 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/010.png', 3, 2, null, 1)
+insert into pokemons values (17, 'Pidgeotto', 'Vuela largas distancias en busca de presas', 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/017.png', 1, 2, null, 1)
+insert into pokemons values (9, 'Blastoise', 'Se agranda para contrarrestar sus chorros de agua', 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/009.png', 3, 2, null, 1)
+insert into pokemons values (19, 'Ratatta', 'Es propenso a morder cualquier cosa', 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/019.png', 1, 3, null, 1)
 
